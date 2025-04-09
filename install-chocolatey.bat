3babff7c97715bc4403ea4751348eeafbb9d6641
MMaffi <121996649+MMaffi@users.noreply.github.com>
2025-04-09 11:46:02 -0300

    Update install-chocolatey.bat

--- install-chocolatey.bat
+++ install-chocolatey.bat
@@ -1,3 +1,5 @@
+@echo off
+
 REM Installing Chocolatey
 @powershell -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 pause
